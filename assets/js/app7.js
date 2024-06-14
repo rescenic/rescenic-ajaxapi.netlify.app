d30a913c5079fde8855f3bf74c06cc266a4e8fb5
Muhammad Ridwan Hakim <adminrescen@gmail.com>
2024-06-14 07:10:34 +0700

    Contoh Async Await

--- assets/js/app7.js
+++ assets/js/app7.js
@@ -18,3 +18,19 @@ getTodos()
 
 console.log(3);
 console.log(4);
+
+//Latihan Async Await
+async function fetchData() {
+  try {
+    const response = await fetch("https://api.example.com/data");
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("There was a problem with the fetch operation:", error);
+  }
+}
+
+fetchData();
